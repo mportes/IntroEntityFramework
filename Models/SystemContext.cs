8a2ea16d37334438485aa9e943088ae4597aafc1
Matheus Portes <65133000+mportes@users.noreply.github.com>
2022-10-05 09:01:22 -0300

    Add files via upload

---
+++ Models/SystemContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace IntroEntityFramework.Models;
+
+public class SystemContext : DbContext
+{
+    public DbSet<Computer> Computers { get; set; }
+
+    public SystemContext() { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySQL("server=localhost;database=estudante;user=estudante;password=estudante;");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using IntroEntityFramework.Models;
+
+Console.WriteLine("Hello, World!");
+
+SystemContext context = new SystemContext();
+
+context.Database.EnsureCreated();
+
+// Create (INSERT)
+// Computer c1 = new Computer(1, "2GB", "13");
+// context.Computers.Add(c1);
+// context.SaveChanges();
+
+// Computer c2 = new Computer(2, "6GB", "i5");
+// context.Computers.Add(c2);
+// context.SaveChanges();
+
+// Computer c3 = new Computer(3, "16GB", "i7");
+// context.Computers.Add(c3);
+// context.SaveChanges();
+
+// Read (SELECT)
+IEnumerable<Computer> computers = context.Computers.ToList();
+foreach (var comp in computers)
+{
+    Console.WriteLine($"{comp.Id}, {comp.Ram}, {comp.Processor}");
+}
+
+// Read (SELECT)
+Computer encontrado = context.Computers.Find(3);
+Console.WriteLine($"Encontrado com Id 3: {encontrado.Ram}, {encontrado.Processor}");
+
+// Update (UPDATE)
+encontrado.Ram = "10GB";
+encontrado.Processor = "amd";
+context.Computers.Update(encontrado);
+context.SaveChanges();
+
+// Read (SELECT)
+Computer atualizado = context.Computers.Find(3);
+Console.WriteLine($"Atualizado com Id 3: {atualizado.Ram}, {atualizado.Processor}");
+
+// Delete (DELETE)
+context.Computers.Remove(atualizado);
+context.SaveChanges();
